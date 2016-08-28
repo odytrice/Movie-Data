180e062308c862b22eff9d33f9827f77ffbc993a
Ody Mbegbu <odytrice@gmail.com>
2016-08-28 19:17:35 +0100

    Completed Pulling Data from Netflix

---
+++ MovieData/Netflix.fsx
@@ -0,0 +1,29 @@
+﻿#load "scripts/load-references-debug.fsx"
+
+open FSharp.Data
+open System.Text.RegularExpressions
+
+type Netflix = XmlProvider< "http://dvd.netflix.com/Top100RSS" >
+
+type MovieBasics = 
+    { Title : string
+      Summary : string
+      Thumbnail : Option<string> }
+
+let getTop100 () = 
+    let top = Netflix.GetSample()
+    [ for it in top.Channel.Items -> 
+          let regexThumb = Regex("<a[^>]*><img src=\"([^\"]*)\".*>(.*)")
+          let m = regexThumb.Match(it.Description)
+          
+          //Get the Description and Title
+          let descr, thumb = 
+              if m.Success then m.Groups.[2].Value, Some m.Groups.[1].Value
+              else it.Description, None
+
+          { Title = it.Title
+            Summary = descr
+            Thumbnail = thumb } ]
+
+
+getTop100() |> Seq.filter (fun m -> m.Thumbnail = None)
---
+++ MovieData/Scripts/load-references-debug.fsx
@@ -0,0 +1,9 @@
+// Warning: generated file; your changes could be lost when a new file is generated.
+#I __SOURCE_DIRECTORY__
+#r "../../packages/FSharp.Data.2.3.2/lib/net40/FSharp.Data.dll"
+#r "System.Core.dll"
+#r "System.dll"
+#r "System.Drawing.dll"
+#r "System.Numerics.dll"
+#r "System.Windows.Forms.dll"
+#r "System.Xml.Linq.dll"
