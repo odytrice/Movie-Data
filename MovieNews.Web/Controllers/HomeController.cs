e9f443355a380d88dec69551eb07946774368fa3
Ody Mbegbu <odytrice@gmail.com>
2016-09-04 09:24:12 +0100

    Added Movie News Web Application

---
+++ MovieNews.Web/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using MovieNews.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MovieNews.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<ActionResult> Index()
+        {
+            var movies = await Movies.GetLatestMovies();
+            return View(movies);
+        }
+
+        public ActionResult Details(string title)
+        {
+            return View();
+        }
+    }
+}
---
+++ MovieNews.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MovieNews.Web.MvcApplication" Language="C#" %>
